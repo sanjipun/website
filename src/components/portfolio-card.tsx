02408957e2385e5369962838fcb94a325c04f875
Sanji Pun <iamsanjipun@gmail.com>
2024-06-12 22:26:20 +0545

    add portfolio page

--- src/components/portfolio-card.tsx
+++ src/components/portfolio-card.tsx
@@ -1,31 +1,36 @@
 import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, IGatsbyImageData, getImage } from "gatsby-plugin-image";
 import React from "react";
 
-const PortfolioCard = () => {
+const PortfolioCard = ({
+  projectName,
+  projectDetails,
+  link,
+  projectImage,
+}: {
+  projectName: string;
+  projectDetails: string;
+  link: string;
+  projectImage: IGatsbyImageData;
+}) => {
   return (
     <div className="max-w-md w-full mt-10 border border-black shadow-[8px_8px_0px_0px_#101010]">
-      <StaticImage
-        src="../images/computer.jpeg"
-        alt="retro star"
-        layout="fixed"
-        width={390}
-        height={250}
+      <GatsbyImage
+        image={getImage(projectImage) as IGatsbyImageData}
+        alt={projectName}
       />
       <div className="flex flex-col justify-center items-center pb-6">
         <h1 className="text-[#1A4D2E] mt-4 font-semibold text-2xl uppercase">
-          Pyx.is wallet
+          {projectName}
         </h1>
         <p className="text-[#1A4D2E] text-center mt-4 text-lg px-2">
-          Pyxis is a chain-agnostic, multi-layer blockchain solution offering a
-          simple, smart, and secure crypto wallet integrated with PyxelChain’s
-          tools
+          {projectDetails}
         </p>
-        <Link to="https://pyx.is/">
-          <button className="text-lg mt-4 px-6 py-4 bg-[#1A4D2E] text-[#eee0c9] shadow-[8px_8px_0px_0px_#101010]">
+        <a href={link}>
+          <button className="rounded-sm text-lg mt-4 px-6 py-4 bg-[#1A4D2E] text-[#eee0c9] shadow-[8px_8px_0px_0px_#101010]">
             View Website
           </button>
-        </Link>
+        </a>
       </div>
     </div>
   );
--- src/components/portfolio.tsx
+++ src/components/portfolio.tsx
@@ -1,16 +1,55 @@
 import React from "react";
 import PortfolioCard from "./portfolio-card";
+import { graphql, useStaticQuery } from "gatsby";
+import { IGatsbyImageData } from "gatsby-plugin-image";
 
 const Portfolio = () => {
+  const sourceData = useStaticQuery(dataQuery);
   return (
-    <div className="max-w-screen-2xl w-full m-auto mt-10 p-6 grid grid-cols-3 items-center justify-items-center">
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
-      <PortfolioCard />
+    <div className="max-w-screen-2xl w-full m-auto my-10 p-6 grid grid-cols-3 items-center justify-items-center">
+      {sourceData.allProjectArrJson.nodes.map(
+        (project: {
+          projectName: string;
+          projectDetails: string;
+          link: string;
+          projectImage: IGatsbyImageData;
+        }) => {
+          return (
+            <PortfolioCard
+              key={project.projectName}
+              projectName={project.projectName}
+              projectDetails={project.projectDetails}
+              link={project.link}
+              projectImage={project.projectImage}
+            />
+          );
+        }
+      )}
     </div>
   );
 };
 
 export default Portfolio;
+
+const dataQuery = graphql`
+  query MyQuery {
+    allProjectArrJson {
+      nodes {
+        projectDetails
+        projectName
+        projectImage {
+          childImageSharp {
+            gatsbyImageData(
+              formats: PNG
+              layout: CONSTRAINED
+              aspectRatio: 2
+              transformOptions: { cropFocus: NORTH }
+              placeholder: TRACED_SVG
+            )
+          }
+        }
+        link
+      }
+    }
+  }
+`;
