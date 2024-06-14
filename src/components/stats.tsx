b2a8315fa6ef80096cfd46622381654c10cd4644
Sanji Pun <iamsanjipun@gmail.com>
2024-06-14 13:55:58 +0545

    update code

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -1,44 +1,5 @@
 import React, { useRef } from "react";
-import { FaFlag } from "@react-icons/all-files/fa/FaFlag";
-import { BsPersonFill } from "@react-icons/all-files/bs/BsPersonFill";
-import { FaStar } from "@react-icons/all-files/fa/FaStar";
-import { GiOfficeChair } from "@react-icons/all-files/gi/GiOfficeChair";
 import { animate } from "framer-motion";
-
-type CustomIconType = React.FC<React.SVGProps<SVGSVGElement>>;
-
-const StatsDetails: { title: string; count: number; icon: CustomIconType }[] = [
-  {
-    title: "Projects",
-    count: 28,
-    icon: FaFlag,
-  },
-  {
-    title: "Clients",
-    count: 12,
-    icon: BsPersonFill,
-  },
-  {
-    title: "YOE",
-    count: 5,
-    icon: FaStar,
-  },
-  {
-    title: "Interns Mentored",
-    count: 22,
-    icon: BsPersonFill,
-  },
-  {
-    title: "Companies",
-    count: 2,
-    icon: GiOfficeChair,
-  },
-  {
-    title: "Partner",
-    count: 1,
-    icon: BsPersonFill,
-  },
-];
 
 function NumberCounter({ from, to }: { from: number; to: number }) {
   const nodeRef = useRef<HTMLParagraphElement>(null);
